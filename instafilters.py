7fd4928bbb611d93eda0c31ec448ddef60184601
poojashetty <poojajkv30@gmail.com>
2019-07-17 20:00:15 +0530

    my first commit

---
+++ instafilters.py
@@ -0,0 +1,11 @@
+from PIL import Image
+base_img=Image.open("base.jpeg")
+img_filter=Image.open("download.jpeg")
+size=(760,760)
+base_img=base_img.resize(size)
+img_filter=img_filter.resize(size)
+r,g,b=base_img.split()
+R,G,B=img_filter.split()
+im=Image.merge("RGB",(R,g,B))
+im.show()
+#im.save('1_merged.jpg')
