f91b6306cc2c755e3867d44ca2f7a3349281803c
poojashetty <poojajkv30@gmail.com>
2019-07-17 20:06:18 +0530

    my second commit

--- instafilters.py
+++ instafilters.py
@@ -1,7 +1,11 @@
+#import Image from PIL
 from PIL import Image
+#Load Images into objects
 base_img=Image.open("base.jpeg")
-img_filter=Image.open("download.jpeg")
+img_filter=Image.open("download.jpg")
+#set 0/p image size
 size=(760,760)
+#resize all images to o/p size
 base_img=base_img.resize(size)
 img_filter=img_filter.resize(size)
 r,g,b=base_img.split()
